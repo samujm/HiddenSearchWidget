9b709d2c352da95b23a3a2d91febe9959e627468
Samantha González <samujm76@gmail.com>
2022-08-23 23:32:36 -0500

    HiddenSearchWidget

---
+++ script.js
@@ -0,0 +1,11 @@
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+const actives = document.querySelectorAll('.active')
+
+
+btn.addEventListener('click',()=>{
+        search.classList.add('active')
+        input.focus()
+})
+
